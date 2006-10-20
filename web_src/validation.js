c56295f3a259d5dacd53719ecba71c845211c74a
abrooks <abrooks>
2006-10-20 02:44:34 +0000

    Added style sheet, and made preliminary updates for authentication to html file and js file.

--- web_src/validation.js
+++ web_src/validation.js
@@ -44,3 +44,42 @@ function clearFields()
   handle.frmStockApp.maxQty.value="";
 }
 
+
+function authenticate()
+{
+
+   var stock_ID
+   stock_ID = getCookieData('stockApp_ID');
+   if (stock_ID.length == 0) || (stock_ID = null) {
+      var url = 
+      document.href = O
+   } else {
+
+
+   }
+
+   document.href= url;
+
+}
+
+//stolen cookie function
+function getCookieData(label) 
+{
+   var labelLen = label.length
+   var cLen = document.cookie.length
+   var i = 0
+   var cEnd
+   while (i < cLen) {
+       var j = i + labelLen
+       if (document.cookie.substring(i,j) == label) {
+          cEnd = document.cookie.indexOf(";",j)
+	  if (cEnd == -1) {
+	     cEnd = document.cookie.length 
+          } 
+	  return unescape(document.cookie.substring(j,cEnd)
+       }
+       i++
+   }
+   return ""
+
+} 
