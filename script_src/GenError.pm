5263f09cf03d7a89376aa474bf888d5290aee850
abrooks <abrooks>
2007-08-30 22:04:56 +0000

    dummy file for now.

---
+++ script_src/GenError.pm
@@ -0,0 +1,31 @@
+package GenError;
+
+
+
+my %err_codes = ( 
+			101 => "Failed Login",
+			102 => "Database Failure",
+			ERRCOND => undef,
+		);	
+
+
+
+sub new
+{
+   my $class = shift;
+   my $code  = shift; 
+   my $self = \%err_codes;
+   ${$self}{$err_codes}{ERRCOND} = $code;
+   bless ($self, $class);
+   return $self;   
+
+}
+
+
+
+
+
+
+
+
+1;
