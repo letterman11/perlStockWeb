4e3bf0ccd62f52d7b89cae7626cb764874dd9a60
abrooks <abrooks>
2007-10-05 20:56:58 +0000

    Add of some error codes. first genuine use of Error object

--- script_src/Error.pm
+++ script_src/Error.pm
@@ -1,10 +1,17 @@
 package Error;
 
+use strict;
+
+my $true             = 1;
+my $false            = 0;
 
 
 my %err_codes = ( 
 			101 => "Failed Login",
 			102 => "Database Failure",
+			103 => "Session Exists",
+			104 => "No Session Exists for ID",
+			105 => "No Object exists for Session",
 			ERRCOND => undef,
 		);	
 
@@ -12,17 +19,21 @@ my %err_codes = (
 
 sub new
 {
-   my $class = shift;
-   my $code  = shift; 
-   my $self = \%err_codes;
-   ${$self}{$err_codes}{ERRCOND} = $code;
-   bless ($self, $class);
-   return $self;   
+	my $class = shift;
+	my $code  = shift; 
+	my $self = \%err_codes;
+	$self->{ERRCOND} = $code;
+	bless ($self, $class);
+	return $self;   
 
 }
 
 
-
+sub errText
+{
+	my $self = shift;
+	return $self->{$self->{ERRCOND}};
+}
 
 
 
