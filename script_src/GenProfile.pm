b3c8106d6bb0c69859d1a843cbf9e75b375f5bbe
abrooks <abrooks>
2008-01-20 23:47:43 +0000

    file added to accomodate change of profile information

---
+++ script_src/GenProfile.pm
@@ -0,0 +1,70 @@
+package GenProfile;
+
+@ISA = qw(GenView);
+
+use strict;
+use CGI::Carp;
+use DBI;
+
+my $div_register = "../web_src/div_register.fhtm";
+
+sub new
+{
+	my $class = shift;
+	my $self = {};
+	my $profile_arr = shift;	
+
+	$self->{PROFILE} = $profile_arr;
+	bless ($self,$class);
+
+}
+
+sub display
+{
+
+	my $self = shift;
+	my ($userName,$userPass,$firstName,$lastName,
+			$address1,$address2,$zipCode,$phone,
+				$emailAddress,$state,$city) = (0..10);
+
+
+	my $out_buffer = <<"OUT_HTML";
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+  <HEAD>
+    <TITLE>Stock Query Application - Registration </TITLE>
+        <LINK href="/~abrooks/style.css" rel="stylesheet" type="text/css">
+        <script language="Javascript" src="/~abrooks/validation.js"> </script>
+        <script language="Javascript" src="/~abrooks/common.js"> </script>
+	<script language="Javascript" type="text/javascript">	
+	var userName 	= $self->{PROFILE}->[$userName];
+	var userPass 	= $self->{PROFILE}->[$userPass];
+	var firstName 	= $self->{PROFILE}->[$firstName];
+	var lastName 	= $self->{PROFILE}->[$lastName];
+	var address1 	= $self->{PROFILE}->[$address1];
+	var address2 	= $self->{PROFILE}->[$address2];
+	var zipCode 	= $self->{PROFILE}->[$zipCode];
+	var phone 	= $self->{PROFILE}->[$phone];
+	var emailAddress = $self->{PROFILE}->[$emailAddress];
+	var state 	= $self->{PROFILE}->[$state];
+	var city 	= $self->{PROFILE}->[$city];
+	</script>
+    </HEAD>
+  <BODY onLoad="init_profile()">
+
+OUT_HTML
+
+	print $out_buffer;
+	display_header();
+	print StockUtil::slurp_file($div_register);
+	display_footer();
+	$out_buffer = <<"OUT_HTML";	
+	
+ </BODY>
+ </HTML>
+
+OUT_HTML
+	print $out_buffer;
+
+}
