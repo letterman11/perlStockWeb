a3f73b44b9413148f45a64bbff23421b3ed71369
abrooks <abrooks>
2008-01-30 14:42:49 +0000

    Created hash to centralize all the URLs in application

--- cgi-bin/config.pl
+++ cgi-bin/config.pl
@@ -11,3 +11,25 @@
 
 
 #push @::shash, (\%sessionHash0, \%sessionHash1, \%sessionHash2, \%sessionHash3 );
+
+#$::URL_PATHS =  {
+#			BANNER_IMAGE		=> "http://www.dcoda.net/DCBANNER_CROP2.jpg",
+#			STOCKAPP_HTM		=> "http://www.dcoda.net/stockapp.html",
+#			REGISTRATION_HTM	=> "http://www.dcoda.net/registration.html",
+#			DIV_REGISTER_FHM        => "/home/u6/octagonyellow/StockApp/web_src/div_register.fhtm"
+#			MAINSTYLE_CSS		=> "http://www.dcoda.net/style.css",
+#			VALIDATION_JS		=> "http://www.dcoda.net/validation.js"	,
+#			COMMON_JS		=> "http://www.dcoda.net/common.js",
+#		};
+
+
+$::URL_PATHS =  {
+                       BANNER_IMAGE            => "/~abrooks/DCBANNER_CROP2.jpg",
+                       STOCKAPP_HTM            => "/~abrooks/stockapp.html",
+                       REGISTRATION_HTM        => "/~abrooks/registration.html",
+		       DIV_REGISTER_FHM        => "../web_src/div_register.fhtm",
+                       MAINSTYLE_CSS           => "/~abrooks/style.css",
+                       VALIDATION_JS           => "/~abrooks/validation.js",
+                       COMMON_JS               => "/~abrooks/common.js",
+               };
+
